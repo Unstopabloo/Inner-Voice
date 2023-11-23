ba4cfdb449df2cd0f7b0a37b356fb08bf001a4e6
Unstopabloo <pablooyarceramirez@gmail.com>
2023-11-22 23:10:54 -0300

    Added user layout and main header, color correction.

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,24 @@
+import { Avatar } from '@nextui-org/react'
+import { Logout } from '../components/Icons'
+import Range from '../components/Range'
+
 export default function Header() {
   return (
-    <header className="user-info bg-slate-400">user-component</header>
+    <section className="header bg-container">
+      <header className="flex flex-col items-center justify-center h-full gap-6">
+        <Avatar
+          src="https://i.pravatar.cc/150?u=a04258114e29026708c"
+          isBordered
+          className="w-32 h-32"
+        />
+        <div className="max-w-[60%] flex flex-col gap-3 items-center">
+          <h2 className="font-bold text-[20px]">Viviano Uyarto</h2>
+          <div className="flex gap-8">
+            <Logout />
+            <Range range="Yogi" />
+          </div>
+        </div>
+      </header>
+    </section>
   )
 }
--- src/layout/MainContent.jsx
+++ src/layout/MainContent.jsx
@@ -1,5 +1,9 @@
+import MainHeader from '../components/MainHeader'
+
 export default function MainContent() {
   return (
-    <main className='main-content bg-green-800'>main-component</main>
+    <main className="main-content bg-container p-7">
+      <MainHeader />
+    </main>
   )
 }
