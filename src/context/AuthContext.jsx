01205af228c9fe65efd52b8b3ac0ac6e14181c80
Unstopabloo <pablooyarceramirez@gmail.com>
2023-12-05 22:53:38 -0300

    hotfix redirect

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,10 +7,25 @@ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState([])
   const [sounds, setSounds] = useState([])
 
+  const getURL = () => {
+    let url =
+      import.meta.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      import.meta.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      'http://localhost:3000/'
+    // Make sure to include `https://` when not localhost.
+    url = url.includes('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+    return url
+  }
+
   async function signInWithGoogle() {
     try {
       const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: 'google'
+        provider: 'google',
+        options: {
+          redirectTo: getURL()
+        }
       })
 
       if (error)
