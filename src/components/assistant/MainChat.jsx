ad48a2e620791ca6fbfa5e3c7c6b88178b753364
Unstopabloo <pablooyarceramirez@gmail.com>
2023-12-09 23:27:20 -0300

    Add loader

--- src/components/assistant/MainChat.jsx
+++ src/components/assistant/MainChat.jsx
@@ -1,15 +1,20 @@
 import { Button, Input } from '@nextui-org/react'
 import { Send } from '../Icons'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import OptionList from './OptionList'
 import { useAssistantStore } from '../../store/assistantStore'
-// import { chatBot } from '../../server/chat'
 import ChatLog from './ChatLog'
 
 export default function MainChat() {
   const [SVGcolor, setSVGcolor] = useState('#1E1E1E')
-  const { question, setQuestion, setUser, user, chatLog, setChatLog } =
-    useAssistantStore()
+  const {
+    question,
+    setQuestion,
+    setUser,
+    setChatLog,
+    isLoading,
+    setIsLoading
+  } = useAssistantStore()
   const [firstMessage, setFirstMessage] = useState(false)
 
   const chatBot = async question => {
@@ -22,7 +27,6 @@ export default function MainChat() {
         },
         body: JSON.stringify({ content: question })
       })
-      setUser('Assistant')
 
       if (!response.ok) {
         throw new Error('Error en el llamado a la api')
@@ -30,25 +34,25 @@ export default function MainChat() {
 
       const data = await response.json()
       console.log(data)
-      setChatLog(data.response, user)
+      setIsLoading(false)
+      setChatLog(data.response, true) // Reemplaza el último mensaje en chatLog con la respuesta del asistente
     } catch (error) {
       console.log('Error en el llamado a la api: ', error)
     }
   }
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault()
     setUser('User')
     setChatLog(question)
-    chatBot(question)
+    setIsLoading(true)
+    setUser('Assistant')
+    setChatLog('Pensando...', false)
+    await chatBot(question)
+    setIsLoading(false)
     setFirstMessage(true)
     setQuestion('')
   }
-
-  useEffect(() => {
-    console.log('Role: ', user)
-    console.log('Conversacion: ', chatLog)
-  }, [chatLog, user])
 
   return (
     <>
@@ -59,6 +63,7 @@ export default function MainChat() {
         className="flex items-center justify-between gap-4"
       >
         <Input
+          disabled={isLoading ? true : false}
           size="sm"
           radius="full"
           type="text"
@@ -69,6 +74,7 @@ export default function MainChat() {
           onChange={e => setQuestion(e.target.value)}
         />
         <Button
+          disabled={isLoading ? true : false}
           isIconOnly
           onClick={handleSubmit}
           radius="full"
--- src/store/assistantStore.ts
+++ src/store/assistantStore.ts
@@ -4,11 +4,13 @@ interface AssistantStore {
   answer: string
   question: string
   user: string
+  isLoading: boolean
   chatLog: { message: string; user: string }[]
   setAnswer: (answer: string) => void
   setQuestion: (question: string) => void
   setUser: (user: string) => void
   setChatLog: (message: string) => void
+  setIsLoading: (isLoading: boolean) => void
 }
 
 export const useAssistantStore = create<AssistantStore>(set => ({
@@ -16,12 +18,17 @@ export const useAssistantStore = create<AssistantStore>(set => ({
   question: '',
   user: '',
   chatLog: [],
+  isLoading: false,
   setAnswer: answer => set({ answer }),
   setQuestion: question => set({ question }),
   setUser: (user: string) => set({ user }),
-  setChatLog: (message: string) => {
-    set(state => ({
-      chatLog: [...state.chatLog, { message, user: state.user }]
-    }))
+  setIsLoading: (isLoading: boolean) => set({ isLoading }),
+  setChatLog: (message: string, replaceLast: boolean = false) => {
+    set(state => {
+      const newChatLog = replaceLast
+        ? state.chatLog.slice(0, -1).concat({ message, user: state.user })
+        : [...state.chatLog, { message, user: state.user }]
+      return { chatLog: newChatLog }
+    })
   }
 }))
