844f125941b310297ccc9bb754be82a355cd2b02
Unstopabloo <pablooyarceramirez@gmail.com>
2023-12-13 12:19:53 -0300

    no more clg

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -24,7 +24,7 @@ export default function Events() {
   }, [])
 
   const handleNewEvent = () => {
-    console.log('Crear evento')
+    //console.log('Crear evento')
     newEvent(name, description)
   }
 
